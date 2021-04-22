4d9dc316f30cc89bd6a7066979084788ac2cc9a5
Bailey Pearson <bailey@cpu13903895.attlocal.net>
2021-04-21 22:12:45 -0400

    Misc changes
    - add tests for enumerate
    - simplify the enumerate function to be curried
    - add tests for most of the range function
    - fix the range function

--- src/core/enumerate.ts
+++ src/core/enumerate.ts
@@ -2,23 +2,10 @@ import { range } from '../utility/range'
 import { zip2 } from './zip2'
 
 type Enumerated<T> = Iterable<[number, T]>
-export function enumerate<T>(): (arg0: Iterable<T>) => Enumerated<T>
 export function enumerate<T>(iter: Iterable<T>): Enumerated<T>
 
-export function enumerate<T>(
-  iter?: Iterable<T>
-): ((arg0: Iterable<T>) => Enumerated<T>) | Enumerated<T> {
-  function* _enumerate(iter: Iterable<T>): Enumerated<T> {
-    for (const value of zip2(range(), iter)) {
-      yield value
-    }
+export function* enumerate<T>(iter: Iterable<T>): Enumerated<T> {
+  for (const value of zip2(range(), iter)) {
+    yield value
   }
-
-  if (iter === undefined) {
-    return _enumerate
-  }
-
-  return (function*() {
-    yield* _enumerate(iter)
-  })()
 }
--- src/utility/range.ts
+++ src/utility/range.ts
@@ -1,20 +1,20 @@
-export function range(): Iterable<number>
-export function range(stop: number): Iterable<number>
-export function range(start: number, stop: number): Iterable<number>
-export function range(start: number, stop: number, step: number): Iterable<number>
+export function range(): Generator<number>
+export function range(stop: number): Generator<number>
+export function range(start: number, stop: number): Generator<number>
+export function range(start: number, stop: number, step: number): Generator<number>
 
-export function range(startStop?: number, stop?: number, step?: number): Iterable<number> {
+export function range(startStop?: number, stop?: number, step?: number): Generator<number> {
   const _step = step === undefined ? 1 : step
   const _stop = stop === undefined ? (startStop === undefined ? Infinity : startStop) : stop
   const _start = startStop === undefined ? 0 : stop === undefined ? 0 : startStop
 
   return (function*() {
     if (_step < 0) {
-      for (let i = _start; (i += _step); i > _stop) {
+      for (let i = _start; i > _stop; i += _step) {
         yield i
       }
     } else {
-      for (let i = _start; (i += _step); i < _stop) {
+      for (let i = _start; i < _stop; i += _step) {
         yield i
       }
     }
